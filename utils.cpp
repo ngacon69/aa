6595fa8fc4c82a0620967ac404f03e08a14fa7ca
ngacon69 <historyproductions1@gmail.com>
2025-02-19 22:54:22 +0700

    Update utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -1,6 +1,47 @@
 #include "utils.h"
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <iostream>
 
-bool timeExpired(std::chrono::steady_clock::time_point start_time, int seconds) {
-    auto end_time = start_time + std::chrono::seconds(seconds);
-    return std::chrono::steady_clock::now() >= end_time;
+// Hàm thay thế _kbhit()
+int _kbhit() {
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if (ch != EOF) {
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
+}
+
+// Hàm thay thế _getch()
+char _getch() {
+    struct termios oldt, newt;
+    char ch;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
 }
